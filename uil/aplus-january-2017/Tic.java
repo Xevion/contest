f360ff1aa38a09733e34a7d00765c7da578daac0
Xevion <xevioni@yandex.com>
2021-04-15 22:43:36 -0500

    A+ January 2017: Rock, Paper, Scissors & Tic Tac Toe

---
+++ uil/aplus-january-2017/Tic.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Tic() {
+    public static void main() throws FileNotFoundException
+    {
+        Scanner in = new Scanner(new File("./tic.dat"));
+    }
+}
